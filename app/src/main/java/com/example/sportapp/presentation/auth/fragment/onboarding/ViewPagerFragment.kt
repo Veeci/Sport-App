28b92f5df8f517d1ee7e575b60316d4cfeb70e93
Veeci <doanvietquang15102003@gmail.com>
2024-07-29 17:35:10 +0700

    creating onboarding ui

---
+++ app/src/main/java/com/example/sportapp/presentation/auth/fragment/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,25 @@
+package com.example.sportapp.presentation.auth.fragment.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sportapp.R
+
+class ViewPagerFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+
+        )
+
+        return view
+    }
+
+}
---
+++ app/src/main/java/com/example/sportapp/presentation/auth/fragment/onboarding/screen/FirstScreen.kt
@@ -0,0 +1,20 @@
+package com.example.sportapp.presentation.auth.fragment.onboarding.screen
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.sportapp.R
+
+class FirstScreen : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first_screen, container, false)
+    }
+
+}
