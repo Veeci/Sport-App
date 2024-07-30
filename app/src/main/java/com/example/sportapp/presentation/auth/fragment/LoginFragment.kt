f91fc68e05b61098285ef86a21cbc388a4763f54
Veeci <doanvietquang15102003@gmail.com>
2024-07-30 10:56:29 +0700

    added navigations between login-signup

--- app/src/main/java/com/example/sportapp/presentation/auth/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/sportapp/presentation/auth/fragment/LoginFragment.kt
@@ -5,16 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.sportapp.R
+import com.example.sportapp.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFunction(view)
+    }
+
+    private fun setFunction(view: View)
+    {
+        binding.btnLogin.setOnClickListener {
+
+        }
+
+        binding.toRegister.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_signupFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+
     }
 
 }
--- app/src/main/java/com/example/sportapp/presentation/auth/fragment/SignupFragment.kt
+++ app/src/main/java/com/example/sportapp/presentation/auth/fragment/SignupFragment.kt
@@ -5,16 +5,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.example.sportapp.R
+import com.example.sportapp.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
+
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signup, container, false)
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setFunction(view)
+    }
+
+    private fun setFunction(view: View) {
+        binding.btnRegister.setOnClickListener{
+
+        }
+
+        binding.toLogin.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_signupFragment_to_loginFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
